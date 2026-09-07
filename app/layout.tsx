7527c0630e1865887e7e9764f8b7335f9e7b0b0a
tarbipyakurel21 <tarbipyakurel21@gmail.com>
2026-09-06 21:06:02 -0500

    add

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'Tarbi Pyakurel - Resume',
+  title: 'Tarbi Pyakurel',
   description: 'Education, experience, projects, and technical skills from Tarbi Pyakurel’s resume.',
 };
 
